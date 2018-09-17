56318128e17b84173a99e5a3cf14b932587dbbd6
Michael Daiello <michael.daiello@9634.local>
2018-09-17 13:54:04 -0400

    MathExample Update

--- MathExample.py
+++ MathExample.py
@@ -1,2 +1,12 @@
 x = 1 + 2
 print (x)
+#These are commentsin Java.  When you program
+#and I look at your work I always chech your comments.
+x = 1 + 2 #This creates a variable and called x and puts 1 + 2 in it 
+print (x)
+
+#Assighment statement 
+#An assignment statement is any situation where I have
+#<something> = <statement>
+x=x + 1 
+print(x)
