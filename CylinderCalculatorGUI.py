5d73b458d897de86e7a25f8fd77486781477816c
Michael Daiello <michael.daiello@9771.local>
2018-09-25 11:25:56 -0400

    GUI1

---
+++ CylinderCalculatorGUI.py
@@ -0,0 +1,24 @@
+import tkinter as tk 
+
+root = tk.Tk()
+root.title("Volume of a Cylinder")
+
+labr = tk.Label(root, text="raduis")
+labr.pack()
+
+entr = tk.Entry(root)
+entr.pack()
+
+labh = tk.Label(root, text="height")
+labh.pack()
+
+enth = tk.Entry(root)
+enth.pack()
+
+btn = tk.Button(root, text="Submit")
+btn.pack()
+
+output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
+output.pack()
+output.config(state="disabled")
+root.mainloop()
