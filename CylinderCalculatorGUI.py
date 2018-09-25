5d180ed3713cf28c3c92257d30f1bbc6123ef9bf
Michael Daiello <michael.daiello@9771.local>
2018-09-25 11:40:33 -0400

    GUI process

--- CylinderCalculatorGUI.py
+++ CylinderCalculatorGUI.py
@@ -1,4 +1,18 @@
 import tkinter as tk 
+import math
+
+def submit():
+
+	print("Submit pressed")
+	r = float(entr.get())
+	h = float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3)
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
+	output.config(state="disabled")
+
 
 root = tk.Tk()
 root.title("Volume of a Cylinder")
@@ -15,10 +29,20 @@ labh.pack()
 enth = tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text="Submit")
+btn = tk.Button(root, text="Submit", command=submit)
 btn.pack()
 
 output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
 output.pack()
 output.config(state="disabled")
+
+
+
+
+
+
+
+
+#This start your EVENT DRIVEN PROGRAM
 root.mainloop()
+print("END PROGRAM")
