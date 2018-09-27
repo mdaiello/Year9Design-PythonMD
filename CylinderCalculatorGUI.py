a3c4675211bc3cedfc0be044484fe28475246ea0
Michael Daiello <michael.daiello@9771.local>
2018-09-27 13:46:29 -0400

    GUI output

--- CylinderCalculatorGUI.py
+++ CylinderCalculatorGUI.py
@@ -10,15 +10,26 @@ def submit():
 	v = math.pi*r*r*h
 	v = round(v,3)
 	output.config(state="normal")
-	output.insert(tk.INSERT,v)
+
+	output.config(state="normal")
+
+	output.delete(1.0,tk.END)
+	outputValue = " Given Amounts\nraduis: "+str(r)+" units\nheight: "+str(h)+" units\nThe volume is: "+str(v)+ " units cubed \n\n "
+	output.insert(tk.INSERT,outputValue)
 	output.config(state="disabled")
 
 
+#This constructs your root window
 root = tk.Tk()
+#This configurates your root window
 root.title("Volume of a Cylinder")
 
+#Step 1: Create or Construct the element 
 labr = tk.Label(root, text="raduis")
+
+
 labr.pack()
+
 
 entr = tk.Entry(root)
 entr.pack()
@@ -44,5 +55,6 @@ output.config(state="disabled")
 
 
 #This start your EVENT DRIVEN PROGRAM
+#The terminal is waiting for you to start or enter things into the program
 root.mainloop()
 print("END PROGRAM")
