e123ed899fbb2995262fbf4b3d828a1c27f4deb3
Michael Daiello <michael.daiello@9771.local>
2018-10-24 13:56:43 -0400

    Lesson12

---
+++ TkinterLesson12.py
@@ -0,0 +1,11 @@
+num = input("enter a number:")
+num = int(num)
+
+if (num < 100):
+	print("This number is less than 100")
+elif (num > 100):
+	print("This number is greater than 100")
+else:
+	print("This number is 100")
+
+
