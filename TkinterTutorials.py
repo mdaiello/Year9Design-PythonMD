6e36e59a47922fcde823a16849d6603ce471dfc7
Michael Daiello <michael.daiello@9771.local>
2018-10-24 13:57:59 -0400

    Tutorials

---
+++ TkinterTutorials.py
@@ -0,0 +1,19 @@
+
+#This is the first lesson on Tkinter 
+import tkinter as tk
+
+# if you are still working under a Python 2 version, 
+# comment out the previous line and uncomment the following line
+# import Tkinter as tk
+
+root = tk.Tk()
+
+w = tk.Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
+
+
+
+
+
